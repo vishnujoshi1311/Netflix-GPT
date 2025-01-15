7a7d9d144918ee6def4b22661b5fb2fea36654e0
Vishnu joshi <vishnuakj59@gmail.com>
2025-01-15 22:57:13 +0530

    added userslice

--- netflix-gpt/src/Utils/UserSlice.js
+++ netflix-gpt/src/Utils/UserSlice.js
@@ -4,5 +4,16 @@ const userSlice = createSlice(
     {
         name:'user',
         initialState:'null',
+        reducers:{
+            adduser: (state,action) =>{
+                return action.payload;
+            },
+            removeuser:(user,action) => {
+                return null;
+            }
+        }
     }
 )
+
+export const {adduser,removeuser} = userSlice.actions;
+export default userSlice.reducer;
