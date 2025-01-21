3a271b56b205b0324e269cb19e8327bdb787dcba
Vishnu joshi <vishnuakj59@gmail.com>
2025-01-21 20:32:20 +0530

    added P

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -98,6 +98,10 @@ const Login = () => {
       <p className='py-4 cursor-pointer'onClick={toggleSignInForm}>{issignInForm? "New to Netflix? sign up now" : "Already a user? Sign In now"}  
       </p>
 
+
+      <p className='py-4 cursor-pointer'onClick={toggleSignInForm}>{issignInForm? "New to Netflix? sign up now" : "Already a user? Sign In now"}  
+      </p>
+
     </form>
 
     </div>
